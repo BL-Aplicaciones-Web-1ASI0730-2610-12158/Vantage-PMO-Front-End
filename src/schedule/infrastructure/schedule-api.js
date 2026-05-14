76da7ea2231038a82f99b965bbf1d8935b6d1eb4
FulLHous <u202211881@upc.edu.pe>
2026-05-14 18:52:56 -0500

    feat(schedule): implement core module (entity, API, assembler, and store)

---
+++ src/schedule/infrastructure/schedule-api.js
@@ -0,0 +1,15 @@
+import { BaseEndpoint } from '../../shared/infrastructure/base-endpoint.js';
+import { ScheduleAssembler } from './schedule.assembler.js';
+
+class ScheduleApi extends BaseEndpoint {
+    constructor() {
+        super(import.meta.env.VITE_SCHEDULE_ENDPOINT_PATH);
+    }
+
+    async getAllItems() {
+        const data = await super.getAll();
+        return ScheduleAssembler.toEntities(data);
+    }
+}
+
+export const scheduleApi = new ScheduleApi();
