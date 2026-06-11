40ddf208b1cccf4d55d6e309d71e3e45a1735650
Alvaro Rocha <alvarorocha54321@gmail.com>
2026-06-10 19:25:56 -0500

    feat(view-2): include viewtype in sign-in resource

--- src/iam/infrastructure/sign-in.resource.js
+++ src/iam/infrastructure/sign-in.resource.js
@@ -9,11 +9,13 @@ export class SignInResource {
      * @param {string|number} params.id - Authenticated user identifier.
      * @param {string} params.username - Authenticated username.
      * @param {string} params.token - Bearer token.
+     * @param {string} params.viewType - The view configuration for the user.
      */
-    constructor({id, username, email, token}) {
+    constructor({id, username, email, token, viewType}) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.token = token;
+        this.viewType = viewType;
     }
 }
